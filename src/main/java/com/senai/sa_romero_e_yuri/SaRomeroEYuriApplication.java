0eec778361a688679e9c971d709af8e93b99e5ce
Romero_Franco <romerofpsilva@gmail.com>
2026-07-07 19:45:50 -0300

    Initial commit

---
+++ src/main/java/com/senai/sa_romero_e_yuri/SaRomeroEYuriApplication.java
@@ -0,0 +1,13 @@
+package com.senai.sa_romero_e_yuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaRomeroEYuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaRomeroEYuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/sa_romero_e_yuri/SaRomeroEYuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.sa_romero_e_yuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaRomeroEYuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
