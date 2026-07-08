56ecef024ff3a2340c0652f875294438c552fa19
Romero_Franco <romerofpsilva@gmail.com>
2026-07-07 21:35:29 -0300

    Adiciona pasta

--- src/main/java/com/senai/sa_romero_e_yuri/entity/Colaborador.java
+++ src/main/java/com/senai/sa_romero_e_yuri/entity/Colaborador.java
@@ -1,8 +1,34 @@
 package com.senai.sa_romero_e_yuri.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "colaborador")
 public class Colaborador {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_colaborador")
+	private int id;
 	
+	@Column(name = "nome")
+	private String nome;
 	
+	@Column(name = "login")
+	private String login;
+	
+	@Column(name = "senha")
+	private String senha;
+	
+	@Column(name = "perfil")
+	private String perfil;
+	
+	@Column(name = "status")
+	private boolean status;
 	
 }
