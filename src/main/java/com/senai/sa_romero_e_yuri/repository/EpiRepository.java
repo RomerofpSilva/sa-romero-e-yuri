cdfd02f2b74cb4608ba00eb70b600208ecd74ae3
Yuri Budke <yuribudke2006@gmail.com>
2026-07-14 20:33:52 -0300

    Adiciona repository EpiRepository

---
+++ src/main/java/com/senai/sa_romero_e_yuri/repository/EpiRepository.java
@@ -0,0 +1,9 @@
+package com.senai.sa_romero_e_yuri.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.sa_romero_e_yuri.entity.Epi;
+
+public interface EpiRepository extends JpaRepository<Epi, Integer>{
+
+}
