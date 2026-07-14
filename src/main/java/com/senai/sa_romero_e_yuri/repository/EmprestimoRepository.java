77bb1f0ac151a462cc664559daa0bc87515b603b
Yuri Budke <yuribudke2006@gmail.com>
2026-07-14 20:33:07 -0300

    Adiciona Repository EmprestimoRepository

---
+++ src/main/java/com/senai/sa_romero_e_yuri/repository/EmprestimoRepository.java
@@ -0,0 +1,9 @@
+package com.senai.sa_romero_e_yuri.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.sa_romero_e_yuri.entity.Emprestimo;
+
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer>{
+
+}
