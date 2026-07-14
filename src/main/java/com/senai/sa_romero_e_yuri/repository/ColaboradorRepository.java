a1d8ec0d0ddad59058f3cf491ababf32fd1b3ed9
Yuri Budke <yuribudke2006@gmail.com>
2026-07-14 20:32:29 -0300

    Adiciona Repository - ColaboradorRepository

---
+++ src/main/java/com/senai/sa_romero_e_yuri/repository/ColaboradorRepository.java
@@ -0,0 +1,9 @@
+package com.senai.sa_romero_e_yuri.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.sa_romero_e_yuri.entity.Colaborador;
+
+public interface ColaboradorRepository extends JpaRepository<Colaborador, Integer> {
+
+}
